738738c554f10869cceb8e30c1a0f821da978fbd
Evan Garrett <evan@fitfox.de>
2020-06-18 13:29:09 +0200

    First commit

---
+++ sensitive_data.js
@@ -0,0 +1,6 @@
+module.exports = {
+    twitter_application_consumer_key: '',  // API Key
+    twitter_application_secret: '',  // API Secret
+    twitter_user_access_token: '',  // Access Token
+    twitter_user_secret: '',  // Access Token Secret
+}
