d30bc0bc54203d61c490a46e54546d14a13afd77
Evan Garrett <evan@fitfox.de>
2020-06-18 13:43:01 +0200

    For security move values to env variables

--- sensitive_data.js
+++ sensitive_data.js
@@ -1,6 +1,6 @@
 module.exports = {
-    twitter_application_consumer_key: '',  // API Key
-    twitter_application_secret: '',  // API Secret
-    twitter_user_access_token: '',  // Access Token
-    twitter_user_secret: '',  // Access Token Secret
+    twitter_application_consumer_key: process.env.APPLICATION_API_KEY,  // API Key
+    twitter_application_secret: process.env.APPLICATION_API_SECRET,  // API Secret
+    twitter_user_access_token: process.env.USER_ACCESS_TOKEN,  // Access Token
+    twitter_user_secret: process.env.USER_SECRET,  // Access Token Secret
 }
